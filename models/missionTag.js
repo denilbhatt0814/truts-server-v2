8b8d6939ed0111e9871d5eb71ee78d3e1783a5ea
Denil Bhatt <denilbhatt2004@gmail.com>
2023-01-27 12:59:19 +0530

    add: color field in missionTag (RGBA)

--- models/missionTag.js
+++ models/missionTag.js
@@ -11,7 +11,35 @@ const missionTagSchema = new mongoose.Schema({
     id: { type: String },
     secure_url: { type: String },
   },
-  // color: {},
+  color: {
+    rgba: {
+      type: [Number],
+      validate: {
+        validator: (rgbaArray) => {
+          if (rgbaArray.length != 4) return false; // must have R-G-B-A
+          if (
+            !(rgbaArray[0] >= 0 && rgbaArray[0] <= 255) ||
+            !Number.isInteger(rgbaArray[0])
+          )
+            return false; // Red channel range: 0-255
+          if (
+            !(rgbaArray[1] >= 0 && rgbaArray[1] <= 255) ||
+            !Number.isInteger(rgbaArray[1])
+          )
+            return false; // Green channel range: 0-255
+          if (
+            !(rgbaArray[2] >= 0 && rgbaArray[2] <= 255) ||
+            !Number.isInteger(rgbaArray[2])
+          )
+            return false; // Blue channel range: 0-255
+          if (!(rgbaArray[3] >= 0 && rgbaArray[3] <= 1)) return false; // Alpha channel range: 0-1
+          // All good
+          return true;
+        },
+        message: "Please provide a valid URL",
+      },
+    },
+  },
 });
 
 module.exports = { MissionTag: mongoose.model("MissionTag", missionTagSchema) };
