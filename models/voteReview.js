5acc7f13809f9c1b07357aa744786afba5ce12cf
Denil Bhatt <denilbhatt2004@gmail.com>
2023-02-11 10:56:24 +0530

    ADD: vote on review

---
+++ models/voteReview.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const voteReviewSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  review: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "NewReview",
+  },
+  action: {
+    type: String,
+    enum: ["UP_VOTE", "DOWN_VOTE", "UNVOTE"],
+    require: [
+      true,
+      `Please add action for this vote to review. ["UP_VOTE", "DOWN_VOTE"]`,
+    ],
+  },
+});
+
+module.exports = { VoteReview: mongoose.model("VoteReview", voteReviewSchema) };
