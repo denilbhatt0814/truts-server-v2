822da5edb22df5251861ffabc4065f6e175bb11d
Denil Bhatt <denilbhatt2004@gmail.com>
2023-10-26 13:05:36 +0530

    ADD: basic event controller and routes

---
+++ controllers/trutsEventController.js
@@ -0,0 +1,9 @@
+exports.createTrutsEvent = async (req, res) => {};
+
+exports.getTrutsEvents = async (req, res) => {};
+
+exports.getTrutsEventById = async (req, res) => {};
+
+exports.getEventCountInATag = async (req, res) => {};
+
+exports.getEventCountInACategory = async (req, res) => {};
---
+++ routes/trutsEvent.js
@@ -0,0 +1,20 @@
+const trutsEventController = require("../controllers/trutsEventController");
+const { isLoggedIn } = require("../middlewares/user");
+const paginateRequest = require("../middlewares/paginate");
+const router = require("express").Router();
+
+router
+  .route("/truts-event")
+  .get(paginateRequest(TrutsEvent, {}, []), trutsEventController.getTrutsEvents)
+  .post(trutsEventController.createTrutsEvent);
+
+// get tags
+router.route("/truts-event/tags").get(trutsEventController.getEventCountInATag);
+router
+  .route("/truts-event/category")
+  .get(trutsEventController.getEventCountInACategory);
+
+// get truts event by id
+router.route("/truts-event/:id").get(trutsEventController.getTrutsEventById);
+
+module.exports = router;
